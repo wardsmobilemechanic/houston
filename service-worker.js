cc7752ec5e6939b713702be3ca27c537092c2176
wardsmobilemechanic <30545057+wardsmobilemechanic@users.noreply.github.com>
2018-01-07 22:20:23 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,27 @@
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  '/',
-  '/style.css',
+  './', // Alias for index.html
+   '/style.css',
   '/contactform.js',
   '/flickity/js',
   '/manifest.json',
@@ -16,8 +32,8 @@ const PRECACHE_URLS = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(PRECACHE)
-    .then(cache => cache.addAll(PRECACHE_URLS))
-    .then(self.skipWaiting())
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
 });
 
