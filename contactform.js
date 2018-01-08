40b61d4efb1c012be6d95e974ecd86b56b04a745
wardsmobilemechanic <30545057+wardsmobilemechanic@users.noreply.github.com>
2018-01-07 23:44:01 -0600

    Update contactform.js

--- contactform.js
+++ contactform.js
@@ -1,16 +1,1 @@
-$('form#contact_form').validate({
-  messages: { },
-  submitHandler: function(form) {
-    $.ajax({
-      url: "https://formspree.io/wardsmobilemechanic@gmail.com",
-      method: "POST",
-      data: $(form).serialize(),
-      dataType: "json",
-      success: function(data) {
-        $("form#contact_form :input").prop("disabled", true);
-        $('#thanks').show();
-      }
-    });
-    return false;
-  }
-});
+$("form#contact_form").validate({messages:{},submitHandler:function(a){return $.ajax({url:"https://formspree.io/wardsmobilemechanic@gmail.com",method:"POST",data:$(a).serialize(),dataType:"json",success:function(a){$("form#contact_form :input").prop("disabled",!0),$("#thanks").show()}}),!1}});
